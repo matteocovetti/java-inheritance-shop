1ebbf24432784f20121f39123d432f6172b500f0
matteocovetti <covetti.matteo@gmail.com>
2022-06-22 16:46:45 +0200

    add prodotto

--- src/jana60/Prodotto.java
+++ src/jana60/Prodotto.java
@@ -1,5 +1,7 @@
 package jana60;
 
+import java.text.DecimalFormat;
+
 public class Prodotto {
 	/*
 	 * Creare la classe Prodotto che gestisce i prodotti dello shop. Un prodotto è
@@ -17,4 +19,78 @@ public class Prodotto {
 	 * classi che gestiscono i vari sotto tipi di prodotto e testate le funzionalità
 	 * delle vostre classi in una classe Main con metodo main.
 	 */
+
+	DecimalFormat df = new DecimalFormat("0.00€");
+
+	// attributi
+	private int codice;
+	private String nome;
+	private String marca;
+	private double prezzo;
+	private int iva;
+
+	// costruttori
+	public Prodotto(int codice, String nome, String marca, double prezzo, int iva) {
+		super();
+		this.codice = codice;
+		this.nome = nome;
+		this.marca = marca;
+		this.prezzo = prezzo;
+		this.iva = iva;
+	}
+
+	// getter e setter
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public double getPrezzo() {
+		return prezzo;
+	}
+
+	void setPrezzo(double prezzo) {
+		if (prezzo < 0.0) {
+			this.prezzo = -prezzo;
+		} else {
+			this.prezzo = prezzo;
+		}
+
+	}
+
+	public int getIva() {
+		return iva;
+	}
+
+	public void setIva(int iva) {
+		this.iva = iva;
+	}
+
+	public int getCodice() {
+		return codice;
+	}
+
+	// metodi
+	public double calcolaPrezzoConIva() {
+		return prezzo + prezzo * iva / 100;
+	}
+
+	// override
+	@Override
+	public String toString() {
+		return "Grazie per aver aquistato il prodotto " + nome + " della marca " + marca + " al prezzo di "
+				+ df.format(calcolaPrezzoConIva());
+	}
+
 }
