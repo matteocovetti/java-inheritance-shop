f9b02f192b6d1e3ad18968eaabc1882cb9655037
matteocovetti <covetti.matteo@gmail.com>
2022-06-22 17:17:27 +0200

    add cuffie

--- src/jana60/Cuffie.java
+++ src/jana60/Cuffie.java
@@ -1,5 +1,45 @@
 package jana60;
 
-public class Cuffie {
+public class Cuffie extends Prodotto {
+
+	// attributi
+	private String colore;
+	private boolean wireless;
+
+	// costruttore
+	public Cuffie(int codice, String nome, String marca, double prezzo, int iva, String colore, boolean wireless) {
+		super(codice, nome, marca, prezzo, iva);
+		this.colore = colore;
+		this.wireless = wireless;
+	}
+
+	// getter e setter
+	public String getColore() {
+		return colore;
+	}
+
+	public void setColore(String colore) {
+		this.colore = colore;
+	}
+
+	public boolean isWireless() {
+		return wireless;
+	}
+
+	public void setWireless(boolean wireless) {
+		this.wireless = wireless;
+	}
+
+	// override
+	@Override
+	public String toString() {
+		String categoria = "";
+		if (wireless) {
+			categoria = "Wireless";
+		} else {
+			categoria = "Cablate";
+		}
+		return super.toString() + "Colore cuffie " + colore + " - " + categoria;
+	}
 
 }
