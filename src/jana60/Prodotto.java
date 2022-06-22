dd0561f25194a065a753903f037e5e6d9ea15208
matteocovetti <covetti.matteo@gmail.com>
2022-06-22 16:27:41 +0200

    first commit

---
+++ src/jana60/Prodotto.java
@@ -0,0 +1,20 @@
+package jana60;
+
+public class Prodotto {
+	/*
+	 * Creare la classe Prodotto che gestisce i prodotti dello shop. Un prodotto è
+	 * caratterizzato da: - codice (numero intero) - nome - marca - prezzo - iva
+	 * Usate opportunamente i livelli di accesso (public, private), i costruttori, i
+	 * metodi getter e setter ed eventuali altri metodi di “utilità” per fare in
+	 * modo che: - il codice prodotto sia accessibile solo in lettura - gli altri
+	 * attributi siano accessibili sia in lettura che in scrittura - il prodotto
+	 * esponga un metodo per avere il prezzo comprensivo di iva formattato Lo shop
+	 * gestisce diversi tipi di prodotto: - Smarphone, caratterizzati anche dal
+	 * codice IMEI e dalla quantità di memoria - Televisori, caratterizzati dalle
+	 * dimensioni e dalla proprietà di essere smart oppure no - Cuffie,
+	 * caratterizzate dal colore e se sono wireless o cablate Utilizzate
+	 * l’ereditarietà per riutilizzare il codice di Prodotto nella stesura delle
+	 * classi che gestiscono i vari sotto tipi di prodotto e testate le funzionalità
+	 * delle vostre classi in una classe Main con metodo main.
+	 */
+}
